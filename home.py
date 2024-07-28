06e7b66f7c520127c2db1b7723dcbb58c2b6feab
sarvesh kesharwani <sarveshkumar5513@gmail.com>
2024-07-28 14:56:54 +0530

    the major code for app is complete

--- home.py
+++ home.py
@@ -1,9 +1,41 @@
 import streamlit as st
 import pandas
 import pickle
-
-# tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
-# model = pickle.load(open('model.pkl', 'rb'))
+import string
+import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
 
 st.title('Email/SMS Spam Classifier')
-sms = st.text_input('Enter the message')
+input_sms = st.text_input('Enter the message')
+
+def transform_text(text):
+    translator = str.maketrans('', '', string.punctuation)
+    ps = PorterStemmer()
+
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    clean_text=[]
+    for word in text:
+        if word not in stopwords.words('english'):
+            word = word.translate(translator)
+            if word.isalnum():
+                clean_text.append(ps.stem(word))
+    return ' '.join(clean_text)
+
+tfidf = pickle.load(open(r'C:\Users\sarvesh.kesharwani\Pictures\DSC\Spam_Classifier\artifacts\trained_vectorizer.pkl', 'rb'))
+model = pickle.load(open(r'C:\Users\sarvesh.kesharwani\Pictures\DSC\Spam_Classifier\artifacts\model_mnb.pkl', 'rb'))
+
+if st.button('Predict'):
+    input_sms = transform_text(input_sms)
+    vectored_sms = tfidf.transform([input_sms])
+    result = model.predict(vectored_sms)[0]
+    if result == 1:
+        st.header("Spam!")
+    else:
+        st.header("Not Spam")
+
+tfidf = pickle.load(open(r'C:\Users\sarvesh.kesharwani\Pictures\DSC\Spam_Classifier\artifacts\tfidf_vectorizer.pkl', 'rb'))
+model = pickle.load(open(r'C:\Users\sarvesh.kesharwani\Pictures\DSC\Spam_Classifier\artifacts\model_mnb.pkl', 'rb'))
+
+
