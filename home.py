2dfcc8f64f72efa6d79e56468c6dfd97c7ec3e0d
sarvesh kesharwani <sarveshkumar5513@gmail.com>
2024-07-28 12:42:50 +0530

    initiated some app code | added some pkgs to requirements file

--- home.py
+++ home.py
@@ -1,2 +1,9 @@
 import streamlit as st
+import pandas
+import pickle
 
+# tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+# model = pickle.load(open('model.pkl', 'rb'))
+
+st.title('Email/SMS Spam Classifier')
+sms = st.text_input('Enter the message')
