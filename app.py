92a2244d9b030dd287e81fbeda100d60fcb07444
Restyled.io <commits@restyled.io>
2024-07-28 11:22:29 +0000

    Restyled by black

--- app.py
+++ app.py
@@ -5,32 +5,33 @@ import pandas
 import pickle
 import string
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
 
-st.title('Email/SMS Spam Classifier')
-input_sms = st.text_input('Enter the message')
+nltk.download("punkt")
+nltk.download("stopwords")
+
+st.title("Email/SMS Spam Classifier")
+input_sms = st.text_input("Enter the message")
 
 
 def transform_text(text):
-    translator = str.maketrans('', '', string.punctuation)
+    translator = str.maketrans("", "", string.punctuation)
     ps = PorterStemmer()
 
     text = text.lower()
     text = nltk.word_tokenize(text)
     clean_text = []
     for word in text:
-        if word not in stopwords.words('english'):
+        if word not in stopwords.words("english"):
             word = word.translate(translator)
             if word.isalnum():
                 clean_text.append(ps.stem(word))
-    return ' '.join(clean_text)
+    return " ".join(clean_text)
 
 
-tfidf = pickle.load(open(r'artifacts/tfidf_vectorizer.pkl', 'rb'))
-model = pickle.load(open(r'artifacts/model_mnb.pkl', 'rb'))
+tfidf = pickle.load(open(r"artifacts/tfidf_vectorizer.pkl", "rb"))
+model = pickle.load(open(r"artifacts/model_mnb.pkl", "rb"))
 
-if st.button('Predict'):
+if st.button("Predict"):
     input_sms = transform_text(input_sms)
     vectored_sms = tfidf.transform([input_sms])
     result = model.predict(vectored_sms)[0]
